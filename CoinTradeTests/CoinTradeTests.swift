aaad16aeac389dd2d928cc57412a774adfa68a9c
Larry <larryworking@gmail.com>
2025-08-17 21:32:11 +0800

    init
    creat project and add icon.

---
+++ CoinTradeTests/CoinTradeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTradeTests.swift
+//  CoinTradeTests
+//
+//  Created by LiuHongfeng on 2025/8/17.
+//
+
+import Testing
+@testable import CoinTrade
+
+struct CoinTradeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
